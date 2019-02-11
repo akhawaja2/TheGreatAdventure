9931328aff40ff118ed17837147726d8b0f2f0ff
Abu <akhawaja@ramapo.edu>
2019-02-11 09:45:00 -0500

    Initial Commit
    aaa

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+    public float moveSpeed;
+
+    public Animator myAnim;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        myAnim.SetFloat("moveX", rigidBody.velocity.x);
+        myAnim.SetFloat("moveY", rigidBody.velocity.y);
+
+        //Checking if horizontal/verticla axis is equal to 1
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            //Player is pressing in that direction
+            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+    }
+}
